24733d68902fdd0830dbc79d0d9557213b3e3e5b
GitDeveloperZ <35308088+GitDeveloperZ@users.noreply.github.com>
2018-01-11 23:04:36 -0500

    Style & interaction using jQuery
    jQuery changes

--- src/app/configuration/configuration-list.component.ts
+++ src/app/configuration/configuration-list.component.ts
@@ -3,6 +3,7 @@ import { ConfigurationService } from './configuration.service';
 import { RestEndPoints } from '../RestEndpoints';
 import { Configuration } from './configuration';
 import { Observable } from 'rxjs/Observable';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'configuration-list',
@@ -17,9 +18,34 @@ export class ConfigurationListComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.configService.getConfigurations(RestEndPoints.download_host + '2')
+        this.configService.getConfigurations(RestEndPoints.download_host + '4')
         .subscribe(
            configs => this.configurations = configs
         );
+
+        $(function() {
+           // Set table style
+           $('table').css('width', '100%');
+           $('table').css('border-collapse', 'collapse');
+
+           // Set table heading and content
+           const elements: string[] = ['td', 'th'];
+           $.each(elements, function(index, element){
+                $(element).css('border', '1px solid #dddddd');
+                $(element).css('padding', '4px');
+                $(element).css('text-align', 'left');
+                if (element === 'th') {
+                    $(element).css('background-color', '#1ABC9C');
+                }
+           });
+
+           // Set row styling
+           $('tr:nth-child(odd)').css('background-color', '#D1F2EB');
+        });
+    }
+
+    deleteConfiguration(index: number): void {
+        // Simulate delete by hiding config row
+        $('#configTableRow' + index).hide();
     }
 }
